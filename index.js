00780757cc0a9bf8570819652ed080490ce97fca
CliffCrerar <cliff.crerar@gmail.com>
2019-02-10 23:57:35 +0200

    Added 5 more themes

--- index.js
+++ index.js
@@ -8,24 +8,34 @@
 
 import CinnabarApplePronghorn from "./Cinnabar Apple Pronghorn.json";
 import DenimGorseWeasel from "./Denim Gorse Weasel.json";
+import DodgerBlueRazzmatazzAardwolf from "./Dodger Blue Razzmatazz Aardwolf.json";
+import DodgerBlueRazzmatazzMatschiesTreeKangaroo from "./Dodger Blue Razzmatazz Matschies Tree Kangaroo.json;";
+import DodgerBlueRazzmatazzPython from "./Dodger Blue Razzmatazz Python.json";
 import OuterSpaceSilverChaliceKiwaHirsuta from "./Outer Space Silver Chalice Kiwa Hirsuta.json";
 import PastelPinkSungloMiniDonkey from "./Pastel Pink Sunglo Mini Donkey.json";
 import PomegranateDenimPigeon from "./Pomegranate Denim Pigeon.json";
 import PomegranateSilverChaliceDonkey from "./Pomegranate Silver Chalice Donkey.json";
 import PomegranateFiordSlothBear from "./Pomegranate Fiord Sloth Bear.json";
 import PomegranatePictonBlueCardinal from "./Pomegranate Picton Blue Cardinal.json";
+import PurpleHeartDodgerBlueChipmunk from "./Purple Heart Dodger Blue Chipmunk.json";
+import PomegranateSunshadeCoquerelsSifaka from "./Pomegranate Sunshade Coquerels Sifaka.json";
 import SanMarinoRazzmatazzRats_default_theme from "./San Marino Razzmatazz Rats.json";
 import SeanceMineShaftHoney from "./Seance Mine Shaft Honey.json";
 
 const themes = [
   CinnabarApplePronghorn,
   DenimGorseWeasel,
+  DodgerBlueRazzmatazzAardwolf,
+  DodgerBlueRazzmatazzMatschiesTreeKangaroo,
+  DodgerBlueRazzmatazzPython,
   OuterSpaceSilverChaliceKiwaHirsuta,
   PastelPinkSungloMiniDonkey,
   PomegranateDenimPigeon,
   PomegranateSilverChaliceDonkey,
   PomegranateFiordSlothBear,
   PomegranatePictonBlueCardinal,
+  PomegranateSunshadeCoquerelsSifaka,
+  PurpleHeartDodgerBlueChipmunk,
   SanMarinoRazzmatazzRats_default_theme,
   SeanceMineShaftHoney
 ];
