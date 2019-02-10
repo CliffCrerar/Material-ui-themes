c8d1b620065c3459bd971c4a3c33e03fea985547
CliffCrerar <cliff.crerar@gmail.com>
2019-02-10 23:07:23 +0200

    Added more themes

--- index.js
+++ index.js
@@ -6,10 +6,33 @@
 //Pomegranate Picton Blue Cardinal
 //Seance Mine Shaft Honey
 
-import CinnabarApplePronghorn from './Cinnabar Apple Pronghorn.json';
-import DenimGorseWeasel from './Denim Gorse Weasel.json';
-import PastelPinkSungloMiniDonkey from './Pastel Pink Sunglo Mini Donkey.json';
-import PomegranateDenimPigeon from './Pomegranate Denim Pigeon.json';
-import PomegranateFiord_Sloth_Bear from './Pomegranate Fiord Sloth Bear.json';
-import PomegranatePictonBlueCardinal from './Pomegranate Picton Blue Cardinal.json';
-import SeanceMineShaftHoney from './Seance Mine Shaft Honey.json';
+import CinnabarApplePronghorn from "./Cinnabar Apple Pronghorn.json";
+import DenimGorseWeasel from "./Denim Gorse Weasel.json";
+import OuterSpaceSilverChaliceKiwaHirsuta from "./Outer Space Silver Chalice Kiwa Hirsuta.json";
+import PastelPinkSungloMiniDonkey from "./Pastel Pink Sunglo Mini Donkey.json";
+import PomegranateDenimPigeon from "./Pomegranate Denim Pigeon.json";
+import PomegranateSilverChaliceDonkey from "./Pomegranate Silver Chalice Donkey.json";
+import PomegranateFiordSlothBear from "./Pomegranate Fiord Sloth Bear.json";
+import PomegranatePictonBlueCardinal from "./Pomegranate Picton Blue Cardinal.json";
+import SanMarinoRazzmatazzRats_default_theme from "./San Marino Razzmatazz Rats.json";
+import SeanceMineShaftHoney from "./Seance Mine Shaft Honey.json";
+
+const themes = [
+  CinnabarApplePronghorn,
+  DenimGorseWeasel,
+  OuterSpaceSilverChaliceKiwaHirsuta,
+  PastelPinkSungloMiniDonkey,
+  PomegranateDenimPigeon,
+  PomegranateSilverChaliceDonkey,
+  PomegranateFiordSlothBear,
+  PomegranatePictonBlueCardinal,
+  SanMarinoRazzmatazzRats_default_theme,
+  SeanceMineShaftHoney
+];
+
+// Updated theme topography
+themes.forEach(theme => {
+  theme.typography.useNextVariants = true;
+});
+
+export default themes;
