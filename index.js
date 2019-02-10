9889a143baa2d32341821cfbbcfd2b5366faf6ef
Cliff <cliff.crerar@gmail.com>
2019-02-10 20:42:05 +0200

    Updated index imports

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-import themePallete from './Cinnabar Apple Pronghorn.json';
+//Cinnabar_Apple_Pronghorn
+//Denim Gorse Weasel
+//Pastel Pink Sunglo Mini Donkey
+//Pomegranate Denim Pigeon
+//Pomegranate Fiord Sloth Bear
+//Pomegranate Picton Blue Cardinal
+//Seance Mine Shaft Honey
 
-export default themePallete;
+import CinnabarApplePronghorn from './Cinnabar Apple Pronghorn.json';
+import DenimGorseWeasel from './Denim Gorse Weasel.json';
+import PastelPinkSungloMiniDonkey from './Pastel Pink Sunglo Mini Donkey.json';
+import PomegranateDenimPigeon from './Pomegranate Denim Pigeon.json';
+import PomegranateFiord_Sloth_Bear from './Pomegranate Fiord Sloth Bear.json';
+import PomegranatePictonBlueCardinal from './Pomegranate Picton Blue Cardinal.json';
+import SeanceMineShaftHoney from './Seance Mine Shaft Honey.json';
